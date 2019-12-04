0af004d1bce6d87d1e9f6da73b983e6a448b5dd4
Alexis Bagarre <alexis.bagarre@bordeaux-inp.fr>
2019-12-04 09:57:33 +0100

    M3: fenetrage et bonne reconstruction (addition recouvrement)

--- M3.m
+++ M3.m
@@ -16,25 +16,30 @@ M = reshape(fcno03fz, [N, 2^power_divide])';
 % Avec recouvrement 50%
 B = buffer(fcno03fz, N, N/2, 'nodelay')';
 
-%% Fenetrage
-
-M_win = (M'.*hamming(208))';
-
 %%  Génération du bruit
 db = 5;
 Pbr  = (10^(db/10));
 Psig = sum(fcno03fz.^2)/L;
 sigm = Psig/Pbr;
 
-M_noise = M_win + randn(size(M_win)).*sqrt(sigm);
+M_noise = M + randn(size(M)).*sqrt(sigm);
+B_noise = B + randn(size(B)).*sqrt(sigm);
+
+%% Fenetrage
+
+M_win = (M'.*hamming(N))';
+B_win = (B'.*hamming(N))';
+
 
 %% Reconstruction du signal
 
-V_M = reshape(M_noise', [L,1]);
+V_M = reshape(M_win', [L,1]);
 
 B_M = zeros(N/2,2^(power_divide+1));
-for i = 1:2^(power_divide+1)-1
-    B_M(:,i) = B(i,1:N/2);
+B_M(:,1) = B_win(1,1:N/2);
+for i = 2:2^(power_divide+1)-1
+    B_M(:,i) = B_win(i,1:N/2) + B_win(i-1,(N/2+1):N);
 end
-B_M(:,i+1) = B(i,N/2+1:N);
+B_M(:,i+1) = B_win(i,N/2+1:N);
 B_M = reshape(B_M, [L,1]);
+
